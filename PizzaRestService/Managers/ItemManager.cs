d8afabc4c137bcf4e0b0c774fcee201db98dcfc1
Thomas (Noit) <40148361+Noitcereon@users.noreply.github.com>
2020-10-08 11:33:09 +0200

    GetOne, GetByName and GetByQualitySubString now also work with database.

--- PizzaRestService/Managers/ItemManager.cs
+++ PizzaRestService/Managers/ItemManager.cs
@@ -16,7 +16,7 @@ namespace PracticeRestService.Managers
             Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;
             MultiSubnetFailover=False";
 
-        private readonly List<Item> items = new List<Item>();
+        private List<Item> _items = new List<Item>();
 
         public IList<Item> GetAll()
         {
@@ -28,13 +28,94 @@ namespace PracticeRestService.Managers
                 SqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    items.Add(ReadNextItem(reader));
+                    _items.Add(ReadNextItem(reader));
                 }
                 reader.Close();
                 dbLink.Close();
             }
 
-            return items;
+            return _items;
+        }
+
+        public Item GetOne(int id)
+        {
+            Item itemWithSpecifiedId = new Item();
+            const string sqlQuery = "SELECT * FROM Item WHERE Id = @ItemId";
+            using (SqlConnection dbLink = new SqlConnection(ConnectionString))
+            {
+                using SqlCommand cmd = new SqlCommand(sqlQuery, dbLink);
+                cmd.Parameters.AddWithValue("@ItemId", id);
+
+                dbLink.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    itemWithSpecifiedId = ReadNextItem(reader);
+                }
+                reader.Close();
+                dbLink.Close();
+            }
+
+            return itemWithSpecifiedId;
+        }
+
+        public IEnumerable<Item> GetByNameSubstring(string substring)
+        {
+            _items = new List<Item>(GetAll());
+
+            return _items.FindAll(x => x.Name.ToLower().Contains(substring.ToLower()));
+        }
+
+        public IEnumerable<Item> GetByQualitySubstring(string substring)
+        {
+            _items = new List<Item>(GetAll());
+
+            return _items.FindAll(x => x.Quality.ToLower().Contains(substring.ToLower()));
+        }
+
+        public IEnumerable<Item> GetWithFilter(FilterItem filter)
+        {
+            List<Item> filteredList = new List<Item>();
+
+            // TODO: Refactor
+            if (filter.HighCost > 0 && filter.LowCost > 0)
+            {
+                filteredList = _items.FindAll((x) => x.Price <= filter.HighCost && x.Price >= filter.LowCost);
+            }
+            else if (filter.HighCost > 0 && filter.LowCost == 0)
+            {
+                filteredList = _items.FindAll(x => x.Price <= filter.HighCost);
+            }
+            else if (filter.HighCost == 0 && filter.LowCost > 0)
+            {
+                filteredList = _items.FindAll(x => x.Price >= filter.LowCost);
+            }
+
+            return filteredList;
+        }
+
+        public String Post(Item newItem)
+        {
+            _items.Add(newItem);
+            return $"{ newItem } added.";
+        }
+
+        public String Put(Item updatedItem, int id)
+        {
+            var oldItem = _items.First(x => x.Id == id);
+            oldItem.Name = updatedItem.Name;
+            oldItem.Quality = updatedItem.Quality;
+            oldItem.Price = updatedItem.Price;
+
+            return $"Updated item Nr. {id}";
+        }
+
+        public String Delete(int id)
+        {
+            Item itemToDelete = GetOne(id);
+            _items.Remove(itemToDelete);
+
+            return $"Deleted item: {itemToDelete}";
         }
 
         private Item ReadNextItem(SqlDataReader reader)
@@ -48,66 +129,6 @@ namespace PracticeRestService.Managers
             };
 
             return nextItem;
-        }
-
-        public Item GetOne(int id)
-        {
-            return items.Find((item) => item.Id == id);
-        }
-
-        public IEnumerable<Item> GetByNameSubstring(string substring)
-        {
-            return items.FindAll(x => x.Name.ToLower().Contains(substring.ToLower()));
-        }
-
-        public IEnumerable<Item> GetByQualitySubstring(string substring)
-        {
-            return items.FindAll(x => x.Quality.ToLower().Contains(substring.ToLower()));
-        }
-
-        public IEnumerable<Item> GetWithFilter(FilterItem filter)
-        {
-            List<Item> filteredList = new List<Item>();
-
-            // TODO: Refactor
-            if (filter.HighCost > 0 && filter.LowCost > 0)
-            {
-                filteredList = items.FindAll((x) => x.Price <= filter.HighCost && x.Price >= filter.LowCost);
-            }
-            else if (filter.HighCost > 0 && filter.LowCost == 0)
-            {
-                filteredList = items.FindAll(x => x.Price <= filter.HighCost);
-            }
-            else if (filter.HighCost == 0 && filter.LowCost > 0)
-            {
-                filteredList = items.FindAll(x => x.Price >= filter.LowCost);
-            }
-
-            return filteredList;
-        }
-
-        public String Post(Item newItem)
-        {
-            items.Add(newItem);
-            return $"{ newItem } added.";
-        }
-
-        public String Put(Item updatedItem, int id)
-        {
-            var oldItem = items.First(x => x.Id == id);
-            oldItem.Name = updatedItem.Name;
-            oldItem.Quality = updatedItem.Quality;
-            oldItem.Price = updatedItem.Price;
-
-            return $"Updated item Nr. {id}";
-        }
-
-        public String Delete(int id)
-        {
-            Item itemToDelete = GetOne(id);
-            items.Remove(itemToDelete);
-
-            return $"Deleted item: {itemToDelete}";
         }
     }
 }
